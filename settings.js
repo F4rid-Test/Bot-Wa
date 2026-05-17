39b98e5d7972227e633d0825cd6cc24386a643bb
F4rid <faridh123567@gmail.com>
2026-05-17 21:12:21 +0700

    Add settings.js

---
+++ settings.js
@@ -0,0 +1,11 @@
+global.nameBot = 'Kino Bot'
+global.owner = ['6281223472806', '6281223473806']
+global.idch = '120363399511075764@newsletter'
+global.ownerName = 'Kino'
+global.dev = "KinoX"
+global.prefix = '='
+global.version = '1.0.0'
+global.footer = '© Created by Kino'
+global.sessionName = 'session'
+global.thumb = 'https://i.ibb.co.com/dJVD0FmV/IMG-20260513-063210.jpg'
+global.customPairing = 'WIRELESS' // Custom pairing code, minimal 8 karakter
